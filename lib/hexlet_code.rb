736b263ab37dc6b9412e7d1407e023a5f7a69820
Stanislav <kunitskiisv@gmail.com>
2023-07-19 21:39:03 +0500

    robo fix

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 module HexletCode
   class Error < StandardError; end
+
   # Your code goes here...
   class Tag
     def self.build(name, **params)
